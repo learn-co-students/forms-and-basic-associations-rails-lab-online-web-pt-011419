e7810f455a8e39eafb0f7b6c51652a828c3974d3
blackwellsmith <blackwellsmithiv@gmail.com>
2019-06-12 12:59:20 -0400

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,4 @@
 module ApplicationHelper
+    has_many :songs
+    has_many :genres, through: :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
 end
